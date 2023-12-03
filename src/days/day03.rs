4a69b74e8fb010c47f79b3f56708b2567f06e3ab
Michael Sickles <michaelsickles@honeycomb.io>
2023-12-03 17:35:39 -0500

    day3

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,5 +1,8 @@
 use crate::{Solution, SolutionPair};
-use std::fs::read_to_string;
+use std::{
+    collections::{HashMap, HashSet},
+    fs::read_to_string,
+};
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -16,17 +19,14 @@ pub fn solve() -> SolutionPair {
         data.push(chars);
     }
     let grid = Grid::new(data);
-    let parts = grid.find_parts();
-    //println!("{:?}", parts);
-     let sol1 = parts.into_iter().reduce(|a, b| a + b).unwrap_or(0);
-    
-    //
-
-
-
-    // Your solution here...
-    //let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let (parts, gear_parts) = grid.find_parts();
+    let sol1 = parts.into_iter().reduce(|a, b| a + b).unwrap_or(0);
+    let mut sol2: u32 = 0;
+    for (_, parts) in gear_parts {
+        if parts.len() == 2 {
+            sol2 += parts.into_iter().product::<u32>();
+        }
+    }
 
     (Solution::from(sol1), Solution::from(sol2))
 }
@@ -34,21 +34,23 @@ pub fn solve() -> SolutionPair {
 struct Grid {
     data: Vec<Vec<char>>,
     row_size: usize,
-    col_size: usize
+    col_size: usize,
 }
 
-impl  Grid {
+impl Grid {
     fn new(data: Vec<Vec<char>>) -> Grid {
         let row_size = data.len();
         let col_size = data[0].len();
         Grid {
             data,
             row_size,
-            col_size
+            col_size,
         }
     }
-    fn is_valid_part(&self, r: i32, c: i32) -> HasSet<char> {
+    fn is_valid_part(&self, r: i32, c: i32) -> (bool, HashSet<(i32, i32)>) {
         let mut is_valid = false;
+        let mut gears: HashSet<(i32, i32)> = HashSet::new();
+
         for i in -1..2 {
             for j in -1..2 {
                 if i == 0 && j == 0 {
@@ -56,58 +58,82 @@ impl  Grid {
                 }
                 let r_index = r + i;
                 let c_index = c + j;
-         
-                if r_index < 0 || r_index >= self.row_size as i32 || c_index < 0 || c_index >= self.col_size as i32 {
+
+                if r_index < 0
+                    || r_index >= self.row_size as i32
+                    || c_index < 0
+                    || c_index >= self.col_size as i32
+                {
                     continue;
                 }
                 let curr_char = self.data[r_index as usize][c_index as usize];
-                if curr_char != '.'  && !curr_char.is_digit(10) {
-                    is_valid = true
+                if curr_char != '.' && !curr_char.is_digit(10) {
+                    if curr_char == '*' {
+                        gears.insert((r_index, c_index));
+                    }
 
+                    is_valid = true;
                 }
-                //println!("r:{} c:{} r_i:{} c_i: {} {} {}", r, c, r_index, c_index, curr_char, is_valid);
-
             }
         }
-        is_valid
+        (is_valid, gears)
     }
-    fn find_parts(&self) -> Vec<u32> {
+    fn find_parts(&self) -> (Vec<u32>, HashMap<(i32, i32), Vec<u32>>) {
         let mut parts: Vec<u32> = Vec::new();
-        let mut runningnumber:u32 = 0;
+        let mut runningnumber: u32 = 0;
         let mut is_valid_part = false;
         let mut r_index = 0;
+        let mut gears = HashSet::new();
+        let mut gears_parts: HashMap<(i32, i32), Vec<u32>> = HashMap::new();
         for row in &self.data {
             let mut c_index = 0;
             for c in row {
-
                 if c.is_numeric() {
-                    if self.is_valid_part(r_index, c_index) {
+                    let (is_valid, added_gears) = self.is_valid_part(r_index, c_index);
+                    if is_valid {
                         is_valid_part = true;
+                        gears.extend(added_gears);
                     }
                     runningnumber = runningnumber * 10 + c.to_digit(10).unwrap();
-                } 
+                }
 
                 if c_index as usize == self.col_size - 1 && is_valid_part && runningnumber != 0 {
                     parts.push(runningnumber);
+
+                    for gear in &gears {
+                        gears_parts
+                            .entry(*gear)
+                            .or_insert(Vec::new())
+                            .push(runningnumber);
+                    }
                     runningnumber = 0;
                     is_valid_part = false;
-                } else if  is_valid_part && runningnumber != 0 && !c.is_numeric() {
+                    gears.clear();
+                } else if is_valid_part && runningnumber != 0 && !c.is_numeric() {
                     parts.push(runningnumber);
+                    for gear in &gears {
+                        gears_parts
+                            .entry(*gear)
+                            .or_insert(Vec::new())
+                            .push(runningnumber);
+                    }
                     runningnumber = 0;
                     is_valid_part = false;
+                    gears.clear();
                 } else if c_index as usize == self.col_size - 1 {
                     runningnumber = 0;
                     is_valid_part = false;
+                    gears.clear();
                 } else if runningnumber != 0 && !c.is_numeric() {
                     runningnumber = 0;
                     is_valid_part = false;
+                    gears.clear();
                 }
-                
+
                 c_index += 1;
             }
             r_index += 1;
-  
         }
-        parts
+        (parts, gears_parts)
     }
 }
